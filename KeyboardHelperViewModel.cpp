e7a0b0d1b6e288fd9c719b5cef7f56bae8b0ec9d
Diponker Roy Dibbyo <115203982+thebbyo@users.noreply.github.com>
2026-06-01 14:40:13 +0600

    Update KeyboardHelperViewModel.cpp

--- KeyboardHelperViewModel.cpp
+++ KeyboardHelperViewModel.cpp
@@ -1,68 +1,77 @@
-﻿
 #include "pch.h"
 #include "KeyboardHelperViewModel.h"
-#include "winrt/SamsungNotes.UI.Controls.h"
-using namespace winrt::SamsungNotes::UI::Controls;
+
+using namespace winrt;
+using namespace Windows::Foundation::Collections;
+
 namespace winrt::SamsungNotes::UI::Controls::implementation
 {
     KeyboardHelperViewModel::KeyboardHelperViewModel()
     {
-        // ===== Note Text Edit Shortcuts (left column) =====
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Select next button", L"Tab", L"", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Select previous button", L"Shift", L"Tab", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Add page", L"Ctrl", L"N", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Create new note", L"Ctrl", L"N", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Create text box", L"Ctrl", L"Shift", L"T", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Strikethrough", L"Ctrl", L"/", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Bold", L"Ctrl", L"B", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Italic", L"Ctrl", L"I", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Underline", L"Ctrl", L"U", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Align center", L"Ctrl", L"E", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Align left", L"Ctrl", L"L", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Align right", L"Ctrl", L"R", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Numbered list", L"Ctrl", L"Shift", L"7", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Bullets", L"Ctrl", L"Shift", L"8", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"To-do", L"Ctrl", L"Shift", L"C", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Decrease font size", L"Ctrl", L"[", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Increase font size", L"Ctrl", L"]", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Indent", L"Ctrl", L"M", L"", L""));
-        m_noteTextEditShortcuts.Append(KeyboardShortcut(L"Outdent", L"Ctrl", L"Shift", L"M", L""));
+        m_categoryText[KeyboardShortcutHelperCategory::General] = L"General";
+        m_categoryText[KeyboardShortcutHelperCategory::TextEditShortcut] = L"Text Editing";
+        m_categoryText[KeyboardShortcutHelperCategory::ViewControlShortcut] = L"View Controls";
+        m_categoryText[KeyboardShortcutHelperCategory::PanelControlShortcut] = L"Panel Controls";
 
-        // ===== View Panel Control Shortcuts (right column) =====
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Zoom In", L"Ctrl", L"+", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Zoom Out", L"Ctrl", L"-", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Zoom In/Out", L"Ctrl", L"Mouse wheel scroll", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Pen settings lock", L"Ctrl", L"Alt", L"L", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Switch to editing mode", L"Enter", L"", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Read only mode on/off", L"Alt", L"Enter", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Full screen view on/off", L"Alt", L"F", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Show pen settings", L"Ctrl", L"T", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Open/Close page settings", L"Alt", L"P", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Expand/Shrink audio controls", L"Alt", L"R", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Play/Pause audio file", L"Ctrl", L"'", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Jump back 10 seconds", L"Ctrl", L",", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Jump forward 10 seconds", L"Ctrl", L".", L"", L""));
-        m_noteViewPanelControlShortcuts.Append(KeyboardShortcut(L"Print note", L"Ctrl", L"Shift", L"P", L""));
+        GenerateGrouped(GetTextShortcuts(), m_textGroups);
+        GenerateGrouped(GetViewShortcuts(), m_viewGroups);
     }
-    
-    hstring KeyboardHelperViewModel::KeyboardHelperHeader()
+
+    IObservableVector<GroupInfosList>
+    KeyboardHelperViewModel::NoteTextEditShortcuts()
     {
-        return m_header;
+        return m_textGroups;
     }
-    
-    winrt::Windows::Foundation::Collections::
-        IObservableVector<
-        KeyboardShortcut>
-        KeyboardHelperViewModel::NoteTextEditShortcuts()
+
+    IObservableVector<GroupInfosList>
+    KeyboardHelperViewModel::NoteViewPanelControlShortcuts()
     {
-        return m_noteTextEditShortcuts;
+        return m_viewGroups;
     }
-    
-    winrt::Windows::Foundation::Collections::
-        IObservableVector<
-        KeyboardShortcut>
-        KeyboardHelperViewModel::NoteViewPanelControlShortcuts()
+
+    void KeyboardHelperViewModel::GenerateGrouped(
+        std::vector<KeyboardShortcut> const& shortcuts,
+        IObservableVector<GroupInfosList> const& target)
     {
-        return m_noteViewPanelControlShortcuts;
+        std::map<KeyboardShortcutHelperCategory, GroupInfosList> groups;
+
+        for (auto const& item : shortcuts)
+        {
+            auto cat = item.HelperCategory();
+
+            if (!groups.contains(cat))
+            {
+                GroupInfosList g;
+                g.Key(L"");
+                groups.emplace(cat, g);
+            }
+
+            groups[cat].Add(item);
+        }
+
+        for (auto& [cat, group] : groups)
+        {
+            group.Key(m_categoryText[cat]);
+            target.Append(group);
+        }
+    }
+
+    std::vector<KeyboardShortcut> KeyboardHelperViewModel::GetTextShortcuts()
+    {
+        return
+        {
+            { L"Bold", L"Ctrl", L"B", L"", L"", KeyboardShortcutHelperCategory::TextEditShortcut },
+            { L"Italic", L"Ctrl", L"I", L"", L"", KeyboardShortcutHelperCategory::TextEditShortcut },
+            { L"Underline", L"Ctrl", L"U", L"", L"", KeyboardShortcutHelperCategory::TextEditShortcut }
+        };
+    }
+
+    std::vector<KeyboardShortcut> KeyboardHelperViewModel::GetViewShortcuts()
+    {
+        return
+        {
+            { L"Zoom In", L"Ctrl", L"+", L"", L"", KeyboardShortcutHelperCategory::ViewControlShortcut },
+            { L"Zoom Out", L"Ctrl", L"-", L"", L"", KeyboardShortcutHelperCategory::ViewControlShortcut }
+        };
     }
 }
